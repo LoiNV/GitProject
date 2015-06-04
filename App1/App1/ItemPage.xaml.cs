b208f4ec0af74aab537fa6d763231205bc1bc31a
LoiNV <loinv_c00615@fpt.aptech.ac.vn>
2015-06-04 10:20:47 +0700

    1

---
+++ App1/App1/ItemPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using App1.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ItemPage : Page
+    {
+        public ItemPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Items item = e.Parameter as Items;
+            BitmapImage bm = new BitmapImage(new Uri(@""+item.Path, UriKind.RelativeOrAbsolute));
+            img.Source = bm;
+            txtName.Text = item.Name;
+        }
+    }
+}
---
+++ App1/App1/ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using App1.Model;
+
+namespace App1.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<Items> Item { get; set; }
+
+        public MainViewModel() {
+            this.Item = new ObservableCollection<Items>();
+            Items i1 = new Items { Name = "1.jpg", Path = "/Assets/Images/1.jpg" };
+            Items i2 = new Items { Name = "2.jpg", Path = "/Assets/Images/2.jpg" };
+            Items i3 = new Items { Name = "3.jpg", Path = "/Assets/Images/3.jpg" };
+            Items i4 = new Items { Name = "4.jpg", Path = "/Assets/Images/4.jpg" };
+            Items i5 = new Items { Name = "5.jpg", Path = "/Assets/Images/5.jpg" };
+            Items i6 = new Items { Name = "6.jpg", Path = "/Assets/Images/6.jpg" };
+
+            Item.Add(i1);
+            Item.Add(i2);
+            Item.Add(i3);
+            Item.Add(i4);
+            Item.Add(i5);
+            Item.Add(i6);
+
+        }
+    }
+}
